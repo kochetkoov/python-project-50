434350c87a22953e887bacedf7b63a1145022f68
Алексей <kochetkoov.ao@mail.ru>
2024-11-18 22:33:36 +0400

    Stage 4

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,6 +2,30 @@ import argparse
 import json
 
 
+def generate_diff(file_path1, file_path2):
+    with open(file_path1, 'r') as file1:
+        data1 = json.load(file1)
+
+    with open(file_path2, 'r') as file2:
+        data2 = json.load(file2)
+
+    keys = sorted(set(data1.keys()).union(data2.keys()))
+    diff = []
+
+    for key in keys:
+        if key in data1 and key in data2:
+            if data1[key] == data2[key]:
+                diff.append(f"    {key}: {data1[key]}")
+            else:
+                diff.append(f"  - {key}: {data1[key]}")
+                diff.append(f"  + {key}: {data2[key]}")
+        elif key in data1:
+            diff.append(f"  - {key}: {data1[key]}")
+        elif key in data2:
+            diff.append(f"  + {key}: {data2[key]}")
+
+    return "{\n" + "\n".join(diff) + "\n}"
+
 def main():
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.'
@@ -12,17 +36,8 @@ def main():
 
     # парсим все аргументы
     args = parser.parse_args()
-
-    with open(args.first_file, 'r') as file:
-        first_data = json.load(file)
-
-    with open(args.second_file, 'r') as file:
-        second_data = json.load(file)
-
-    # Пока что просто выводим аргументы для проверки
-    print(f"Comparing '{args.first_file}' and '{args.second_file}'.")
-    print(f'First file data: {first_data}')
-    print(f'Second file data: {second_data}')
+    result = generate_diff(args.first_file, args.second_file)
+    print(result)
 
 
 if __name__ == '__main__':
